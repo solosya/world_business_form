70da7d01612217ceadb6d5ef0ec0d46d49c5fa90
Lee Neenan <lneenan@pagemasters.com.au>
2017-05-30 14:34:31 +1000

    re gulped

--- static/js/user_concat.js
+++ static/js/user_concat.js
@@ -20714,12 +20714,11 @@ var socialCardTemplate =  '<div class="{{containerClass}}">' +
                                     data-social="1"\
                                     data-article-image="{{{social.media.path}}}"\
                                     data-article-text="{{social.content}}">\
-                                    {{#if social.hasMedia}}\
                                     <article class="">\
-                                    \
-                                        <figure class="{{videoClass}}">\
-                                            <img class="img-responsive" src="{{social.media.path}}" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=33&txt=Loading&w=450&h=250)">\
-                                        </figure>\
+                                        {{#if social.hasMedia}}\
+                                            <figure class="{{videoClass}}">\
+                                                <img class="img-responsive" src="{{social.media.path}}" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=33&txt=Loading&w=450&h=250)">\
+                                            </figure>\
                                         {{/if}}\
                                         \
                                         <div class="content">\
@@ -20834,6 +20833,35 @@ var socialPostPopupTemplate =
                     '</div>'+
     '</div>'+
  '</div>'   ;   
+// var ArticleController = (function ($) {
+//     return {
+//         view: function () {
+//             ArticleController.View.init();
+//         }
+//     };
+// }(jQuery));
+
+// ArticleController.View = (function ($) {
+
+//     var attachEvents = function () {
+
+//         var fullExcerptHeight = $('.article_main .news__main').height();
+//         $($('.news__sidebar a.card').get().reverse()).each(function () {
+//             var sidebarHeight = $('.article_main .news__sidebar').height();
+//             if (fullExcerptHeight < sidebarHeight) {
+//                 $(this).addClass('hide');
+//             }
+//         });
+//     };
+
+//     return {
+//         init: function () {
+//             attachEvents();
+//         }
+//     };
+
+// }(jQuery));
+
 var AuthController = (function ($) {
     return {
         loginOrSignup: function () {
@@ -20970,7 +20998,7 @@ Card.prototype.renderScreenCards = function(options, data)
     }
     container.empty().append(html);
 
-    $('.two-card-logo').toggle();
+    // $('.two-card-logo').toggle();
 
     $(".card p, .card h1").dotdotdot();
             
@@ -20996,24 +21024,26 @@ Card.prototype.screen = function()
 
     var options = {
         'screens' : [
-            {
-                style: "screen-card card-sm-screen col-sm-6",
-                limit: 2,
-                logo: "small-logo"
-            }, 
+        {
+            style: "screen-card card-sm-screen col-sm-6",
+            limit: 2,
+            logo: "small-logo"
+        }, 
+
             {
                 style: "screen-card card-lg-screen col-sm-12",
                 limit: 1,
                 logo: "large-logo"
 
-            }],
+            },
+        ],
         'container': $( '#'+btn.data('container') ),
         'currentScreen': currentScreen,
         'count': 20
     };
 
     var run = function() {
-        console.log('running');
+        // console.log('running');
         var numberOfScreens = options.screens.length;
         currentScreen++;
         if (currentScreen > numberOfScreens) {
@@ -21022,12 +21052,12 @@ Card.prototype.screen = function()
         var screenOption = currentScreen-1;
         options.currentScreen = currentScreen;
 
-        console.log('grigidig');
+        // console.log('grigidig');
         options.limit = options.screens[screenOption].limit;
         options.containerClass = options.screens[screenOption].style;
 
         // articleCount = articleCount + options.limit;
-        console.log('Article Count: ', articleCount);
+        // console.log('Article Count: ', articleCount);
         if (articleCount >= options.count) {
             articleCount = 0;
         }
@@ -21035,11 +21065,11 @@ Card.prototype.screen = function()
         options.offset = articleCount;
         options.nonpinned = articleCount;
 
-        console.log(options);
+        // console.log(options);
         $.fn.Ajax_LoadBlogArticles(options).done(function(data) {
-            console.log(data);
+            // console.log(data);
             if (data.articles.length == 0 ) {
-                console.log('setting article count to zero');
+                // console.log('setting article count to zero');
                 articleCount = 0;
                 return;
             }
@@ -21052,7 +21082,7 @@ Card.prototype.screen = function()
     }
 
     run();
-    console.log('setting interval');
+    // console.log('setting interval');
     setInterval ( run, 5000 );  
 };
 
@@ -21447,6 +21477,169 @@ Card.prototype.events = function()
         });
     });
 };
+(function ($) {
+    
+    $('.video-player').videoPlayer();
+    
+    $("div.lazyload").lazyload({
+        effect : "fadeIn"
+    });
+    
+    // $(window).resize(function() {
+    //     if ($('.side-navigation').is(':visible')) {
+    //         var currentWidth = $('.side-navigation').width();
+    //         var windowWidth = $(window).width();
+    //         if (currentWidth > windowWidth && windowWidth > 300) {
+    //             var newWidth = windowWidth - 20;
+    //             $('.side-navigation').css('width', newWidth + 'px');
+    //         }
+    //     }
+    // });
+  
+    // $('.forceLoginModal').loginModal({
+    //     onLoad: function () {
+    //         $("#loginForm").validateLoginForm();
+    //         $("#signupForm").validateSignupForm();
+    //     }
+    // });
+    
+
+    /************************************************************************************
+     *              FOLLOW AND UNFOLLOW ARTICLE PAGE JS
+     ************************************************************************************/
+    // $('.followArticleBtn').followBlog({
+    //     onSuccess: function (data, obj) {
+    //        ($(obj).data('status') === 'follow') ? $(obj).html("Follow +") : $(obj).html("Following -");
+    //         var message = ($(obj).data('status') === 'follow') ? 'Unfollow' : 'Follow';
+    //         $.fn.General_ShowNotification({message: message + " blog successfully."});                 
+    //     },
+    //     beforeSend: function (obj) {
+    //         $(obj).html('please wait...');
+    //     },
+    //     onError: function (obj, errorMessage) {
+    //         $().General_ShowErrorMessage({message: errorMessage});
+    //     }
+    // });
+    
+    /************************************************************************************
+     *              FOLLOW AND UNFOLLOW USER PROFILE PAGE JS
+     ************************************************************************************/
+    
+    // $('.FollowProfileBlog').followBlog({
+    //     onSuccess: function (data, obj) {
+    //         var status = $(obj).data('status');
+    //         if($(obj).hasClass('hasStar')) {
+    //             (status == 'unfollow') ? $(obj).addClass('selected') : $(obj).removeClass('selected');
+    //         }  
+    //     },
+    //     beforeSend: function (obj) {
+    //         $(obj).find('.fa').addClass('fa-spin fa-spinner');
+    //     },
+    //     onError: function (obj, errorMessage) {
+    //         $().General_ShowErrorMessage({message: errorMessage});
+    //     },
+    //     onComplete: function (obj) {
+    //         $(obj).find('.fa').removeClass('fa-spin fa-spinner');
+    //     }
+    // });
+    
+    
+    // $("#owl-thumbnails").owlCarousel({
+    //     items: 2,
+    //     itemsDesktop: [1199, 2],
+    //     itemsDesktopSmall: [980, 1],
+    //     itemsTablet: [768, 1],
+    //     itemsMobile: [600, 1],
+    //     pagination: true,
+    //     navigation: true,
+    //     loop: true,
+    //     autoplay: true,
+    //     autoplayTimeout: 1000,
+    //     navigationText: [
+    //         "<i class='fa fa-angle-left fa-2x'></i>",
+    //         "<i class='fa fa-angle-right fa-2x'></i>"
+    //     ]
+    // });     
+    
+    // $('.shareIcons').SocialShare({
+    //     onLoad: function (obj) {
+    //         var title = obj.parents('div.article').find('.card__news-category').text();
+    //         var url = obj.parents('div.article').find('a').attr('href');
+    //         var content = obj.parents('div.article').find('.card__news-description').text();
+    //         $('.rrssb-buttons').rrssb({
+    //             title: title,
+    //             url: url,
+    //             description: content
+    //         });
+    //         setTimeout(function () {
+    //             rrssbInit();
+    //         }, 10);
+    //     }
+    // });
+    
+    //Contact form validation
+    // $('#contactForm').validate({
+    //     rules: {
+    //         name: "required",
+    //         email: "required",
+    //         message: "required"
+    //     },
+    //     errorElement: "span",
+    //     messages: {
+    //         name: "Name cannot be blank.",
+    //         email: "Email cannot be blank.",
+    //         message: "Message cannot be blank."
+    //     }
+    // });
+    
+    /************************************************************************************
+     *                  USER EDIT PROFILE PAGE JS
+     ************************************************************************************/
+    
+    // $('.uploadFileBtn').uploadFile({
+    //        onSuccess: function(data, obj){
+    //             var resultJsonStr = JSON.stringify(data);
+                
+    //             var imgClass = $(obj).data('imgcls');
+    //             $('.' + imgClass).css('background-image', 'url(' + data.url + ')');
+                
+    //             var fieldId = $(obj).data('id');
+    //             $('#' + fieldId).val(resultJsonStr);
+                
+    //             $().General_ShowNotification({message: 'Image added successfully' });
+    //         }
+    // });
+    
+     /**
+     * Update Social Post From Listing
+     */
+    // $('.editSocialPost').on('click', function (e) {
+    //     e.preventDefault();
+    //     var elem = $(this);
+    //     var url = elem.data('url');
+    //     var popup = window.open(url, '_blank', 'toolbar=no,scrollbars=yes,resizable=false,width=360,height=450');
+    //     popup.focus();
+
+    //     var intervalId = setInterval(function () {
+    //         if (popup.closed) {
+    //             clearInterval(intervalId);
+    //             var socialId = elem.parents('a').data('id');
+    //             if($('#updateSocial'+socialId).data('update') == '1') {
+    //                 $().General_ShowNotification({message: 'Social Post(s) updated successfully.'});
+    //             }  
+    //         }
+    //     }, 50);
+
+    //     return;
+    // });
+    
+}(jQuery));
+
+
+    
+
+
+
 var HomeController = (function ($) {
     return {
         listing: function () {
